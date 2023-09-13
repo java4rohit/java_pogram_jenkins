f8e3afea9febe4a38daaa7b05e72d111559c0495
Rohit Yadav <60982731+java4rohit@users.noreply.github.com>
2023-09-13 13:22:12 +0530

    push

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main{
   public static void main(String[] args){
            System.out.println("This is java Program");
                System.out.println("OutPut java Program");
-               System.out.println(new Date());
+             
            
   }
 }
